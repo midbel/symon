73a8d026761f2f6617cadc953b30bd725313acce
nico <brun.nicolas21@gmail.com>
2017-07-18 20:50:01 +0200

    who handler

--- cmd/symond/symond.go
+++ cmd/symond/symond.go
@@ -6,7 +6,7 @@ import (
 	"fmt"
 	"net/http"
 	"os"
-  "path"
+	"path"
 
 	"github.com/midbel/symon"
 )
@@ -34,24 +34,24 @@ func Process(w http.ResponseWriter, r *http.Request) {
 
 func Users(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
-  var (
-    who interface{}
-    err error
-  )
-  switch _, base := path.Split(r.URL.Path); base {
-  case "", "utmp":
-    who, err = symon.Utmp()
-  case "wtmp":
-    who, err = symon.Wtmp()
-  case "lastlog":
-    who, err = symon.Last()
-  case "faillog":
-    w.WriteHeader(http.StatusNotImplemented)
-    return
-  default:
-    w.WriteHeader(http.StatusNotFound)
-    return
-  }
+	var (
+		who interface{}
+		err error
+	)
+	switch _, base := path.Split(r.URL.Path); base {
+	case "", "utmp":
+		who, err = symon.Utmp()
+	case "wtmp":
+		who, err = symon.Wtmp()
+	case "lastlog":
+		who, err = symon.Last()
+	case "faillog":
+		w.WriteHeader(http.StatusNotImplemented)
+		return
+	default:
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
 		return
