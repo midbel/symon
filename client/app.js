aa163e5e8b0c87580b70580a3b8f3e499aac6f8c
nico <brun.nicolas21@gmail.com>
2018-01-15 21:23:09 +0100

    process list view

---
+++ client/app.js
@@ -0,0 +1,64 @@
+const host = "http://localhost:9090"
+const api = {
+  process: `${host}/process/`,
+}
+
+const criteria = {
+  user: "",
+  group: "",
+  command: "",
+  state: "",
+};
+
+const app = new Vue({
+  el: "#symon",
+  data: {
+    process: [],
+    error: "",
+    interval: undefined,
+    rate: 1000,
+    criteria: criteria,
+  },
+  mounted() {
+    this.interval = setInterval(_.bind(this.update, this), this.rate);
+    this.update();
+  },
+  computed: {
+    all() {
+      return this.process;
+    },
+    users() {
+      return this.extract("user");
+    },
+    groups() {
+      return this.extract("group");
+    },
+    commands() {
+      return this.extract("process");
+    },
+    states() {
+      return this.extract("state");
+    },
+  },
+  methods: {
+    extract(a) {
+      return _.chain(this.process).map(p => p[a]).uniq().sort().value();
+    },
+    update() {
+      fetch(api.process, {headers: {accept: "application/json"}}).then(r => {
+        if (!r.ok) {
+          return Promise.reject(r.statusText());
+        }
+        return r.json();
+      }).then(rs => {
+        this.process = _.sortBy(rs, [d => d.ppid, d => d.pid]);
+        this.error = "";
+      }).catch(err => {
+        this.error = err;
+        if (this.interval) {
+          clearInterval(this.interval);
+        }
+      });
+    },
+  },
+});
